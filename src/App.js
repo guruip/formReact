cc77ee586b3b5588401c0e0cb857c583bfb7fa39
guruip <82904567+guruip@users.noreply.github.com>
2023-05-25 23:42:31 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react"
+import Header from "./components/Header"
+import Users from "./components/Users"
+import AddUser from "./components/AddUser"
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id:1,
+                    firstname:'Bob',
+                    lastname:'Marley',
+                    bio:'lorem A tiny VS Code extension made up of a few commands that generate and insert lorem ipsum text into a text file.',
+                    age:40,
+                    isHappy:true
+                },
+                {
+                    id:2,
+                    firstname:'Jon',
+                    lastname:'Doe',
+                    bio:'lorem A tiny VS Code extension made up of a few commands that generate and insert lorem ipsum text into a text file.',
+                    age:22,
+                    isHappy:false
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+        this.deleteUser = this.deleteUser.bind(this)
+        this.editUser = this.editUser.bind(this)
+    }
+    render() {
+        return (<div>
+            <Header title="Список пользователей" />
+            <main>
+                <Users users={this.state.users} onEdit={this.editUser} onDelete={this.deleteUser}/>
+            </main>
+            <aside>
+                <AddUser onAdd={this.addUser}/>
+            </aside>
+        </div>)
+    }
+
+    deleteUser(id) {
+        this.setState({
+            users: this.state.users.filter((el) => el.id !== id)
+        })
+    }
+
+    editUser(user) {
+        let allUsers = this.state.users
+        allUsers[user.id - 1] = user
+
+        this.setState({users: []}, () => {
+            this.setState({ users: [...allUsers]})
+        })
+    }
+
+    addUser(user) {
+        const id = this.state.users.length + 1
+        this.setState({ users: [...this.state.users, {id, ...user}]})
+    }  
+}
+
+export default App
---
+++ src/components/User.js
@@ -0,0 +1,32 @@
+import React from "react"
+import {IoCloseCircleSharp, IoHammerSharp} from 'react-icons/io5'
+import AddUser from "./AddUser"
+
+class Header extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            editForm: false
+        }
+    }
+    user = this.props.user
+    render() {
+        return (
+            <div className="user">
+                <IoCloseCircleSharp onClick={() => this.props.onDelete(this.user.id)} className="delete-icon"/>
+                <IoHammerSharp onClick={() => {
+                    this.setState({
+                        editForm: !this.state.editForm
+                    })
+                }} className="edit-icon"/>
+                <h3>{this.user.firstname} {this.user.lastname}</h3>
+                <p>{this.user.bio}</p>
+                <b>{this.user.isHappy ? 'Счастлив' : 'Не особо'}</b>
+
+                {this.state.editForm && <AddUser user={this.user} onAdd={this.props.onEdit} /> }
+            </div>
+        )
+    }
+}
+
+export default Header
